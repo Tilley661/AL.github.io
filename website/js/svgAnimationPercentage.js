167d79e98fb7fcbbe58d192796f38cb327915a15
Tilley661 <Tilley661@mail.com>
2019-07-26 13:32:27 +0100

    Update svgAnimationPercentage.js

--- website/js/svgAnimationPercentage.js
+++ website/js/svgAnimationPercentage.js
@@ -71,7 +71,7 @@ function animate(svg){
         if(globals.scrollingDirection === "down"){
             endOpacity = percentageMod + (Math.random() * (end.opacity - start.opacity)); // randomizes the opacity but will eventually be 1
         }else{
-            endOpacity =  (1 - percentageMod) * (Math.random() + 0.25) ; // randomizes the opacity but will eventually be 0
+            endOpacity =  (percentageMod) * (Math.random() + 0.25) ; // randomizes the opacity but will eventually be 0
         }
 
         if(globals.percentage < 0){
